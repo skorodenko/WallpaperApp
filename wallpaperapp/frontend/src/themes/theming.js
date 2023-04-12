9c281b3b7a69ac541fc0f02c17dd50ffa848b1ce
skorodenko <mskorodenko@gmail.com>
2023-04-12 19:40:59 +0300

    Corrected theme

--- wallpaperapp/frontend/src/themes/theming.js
+++ wallpaperapp/frontend/src/themes/theming.js
@@ -2,11 +2,11 @@ export const themes = {
     light: {
         color: "black",
         backgroundColor: "rgb(255,255,255)",
-        blur_bg: "rgba(235,235,235,.7)",
+        blur_bg: "rgba(235,235,235,.75)",
     },
     dark: {
         color: "white",
         backgroundColor: "rgb(0,0,0)",
-        blur_bg: "rgba(35,35,35,.7)",
+        blur_bg: "rgba(25,25,25,.75)",
     },
 };
