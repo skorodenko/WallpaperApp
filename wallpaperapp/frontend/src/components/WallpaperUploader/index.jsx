52684feb7b14faaed770ee40019c5415c0275256
skorodenko <mskorodenko@gmail.com>
2023-04-14 02:53:03 +0300

    Uploading image + tags with axios

--- wallpaperapp/frontend/src/components/WallpaperUploader/index.jsx
+++ wallpaperapp/frontend/src/components/WallpaperUploader/index.jsx
@@ -1,12 +1,16 @@
 import { animated } from "@react-spring/web";
 import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 import Fnav from "components/Fnav"
 import ListImages from "./ListImages";
 import UploaderImage from "./UploaderImage";
 import EditImage from "./EditImage";
+import { clientAxios } from "api/axios";
 
 import styles from "./styles.module.css"
+
+const upload_url = "/images/"
 
 export default function WallpaperUploader({ theme }) {
     const [images, setImages] = useState([])
@@ -17,21 +21,27 @@ export default function WallpaperUploader({ theme }) {
     useEffect(() => {
         if (images.length < 1) return;
         const newImageURLs = []
-        images.forEach(image => newImageURLs.push({
-            url: URL.createObjectURL(image),
-            tags: [],
-        }))
+        images.map(image => {
+            const url = URL.createObjectURL(image)
+            newImageURLs.push({
+                url: url,
+                file: image,
+                tags: [],
+            })
+            return { url: url, file: image }
+        })
         setImageURLs(newImageURLs)
+        setImages([])
     }, [images])
 
-    const removeImage = (image) => {
-        if(image.url === activeImage.url) setActiveImage(null)
-        if(stagedImages.includes(image)) setStagedImages(stagedImages.filter(img => img.url !== image.url))
-        setImageURLs(imageURLs.filter(img => img.url !== image.url))
+    const removeImages = (images) => {
+        setImageURLs(imageURLs.filter(img => !images.includes(img)))
+        setStagedImages(stagedImages.filter(img => !images.includes(img)))
+        if (images.includes(activeImage)) setActiveImage(null)
     }
 
     const stageImage = (image, isStaged) => {
-        if(isStaged) {
+        if (isStaged) {
             // If image is already staged -> unstage
             setStagedImages(stagedImages.filter(img => img.url !== image.url))
         } else {
@@ -44,28 +54,63 @@ export default function WallpaperUploader({ theme }) {
         setImages([...e.target.files])
     }
 
+    const uploadStagedImages = async () => {
+        if(!stagedImages.length) throw new Error("No images were staged to upload")
+        if(stagedImages.some(img => img.tags.length === 0)) throw new Error("One of the staged images has not tags")
+        
+        const uploaded = await Promise.all(stagedImages.map(async image => {
+            return await clientAxios.postForm(upload_url, {
+                image: image.file,
+                tags: image.tags,
+            }).then(() => {
+                return image
+            })
+        }))
+
+        return uploaded
+    }
+
+    const uploadStaged = () => {
+        const promise = uploadStagedImages()
+            .then(uploaded => {
+                removeImages(uploaded)
+                return uploaded.length
+            })
+            .catch(err => {
+                console.log(err)
+                throw err
+            })
+        
+        toast.promise(promise, {
+            loading: "Uploading images ...",
+            success: (quantity) => `Uploaded ${quantity} images`,
+            error: (err) => `${err.message}`,
+        })
+    }
+
     return (
         <animated.div className={styles.page} style={theme}>
             <Fnav theme={theme} />
             <animated.div className={styles.container_grid} style={theme}>
-                <animated.div className={styles.edit_image_container} style={{...theme, backgroundColor: theme.blur_bg}}>
-                    {activeImage && <EditImage 
-                        theme={theme} 
+                <animated.div className={styles.edit_image_container} style={{ ...theme, backgroundColor: theme.blur_bg }}>
+                    {activeImage && <EditImage
+                        theme={theme}
                         image={activeImage}
-                        removeImage={removeImage}
+                        removeImage={() => removeImages([activeImage])}
                         staged={stagedImages.includes(activeImage)}
                         setStaged={stageImage}
                     />}
                 </animated.div>
-                <animated.div className={styles.image_container} style={{...theme}}>
-                    <UploaderImage image={activeImage}/>
+                <animated.div className={styles.image_container} style={{ ...theme }}>
+                    <UploaderImage image={activeImage} />
                 </animated.div>
-                <ListImages 
-                    theme={theme} 
-                    onImageChange={onImageChange} 
-                    imageList={imageURLs} 
+                <ListImages
+                    theme={theme}
+                    onImageChange={onImageChange}
+                    imageList={imageURLs}
                     stagedList={stagedImages}
                     setActiveImage={setActiveImage}
+                    uploadStaged={uploadStaged}
                 />
             </animated.div>
         </animated.div>
